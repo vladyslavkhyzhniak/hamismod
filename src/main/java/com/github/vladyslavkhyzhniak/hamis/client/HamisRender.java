399f46bae530133d5c7f437f9b940b08010f5d25
Vlad Kh <vladyslavkhyzhniak@gmail.com>
2026-01-23 17:22:28 +0100

    Hamis Model structure update

--- src/main/java/com/github/vladyslavkhyzhniak/hamis/client/HamisRender.java
+++ src/main/java/com/github/vladyslavkhyzhniak/hamis/client/HamisRender.java
@@ -2,15 +2,13 @@ package com.github.vladyslavkhyzhniak.hamis.client;
 
 import com.github.vladyslavkhyzhniak.hamis.client.model.HamisModel;
 import com.github.vladyslavkhyzhniak.hamis.entity.HamisEntity;
-import net.minecraft.client.model.SilverfishModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 import static com.github.vladyslavkhyzhniak.hamis.hamis.MOD_ID;
 
-public class HamisRender  extends MobRenderer<HamisEntity, HamisModel> {
+public class HamisRender  extends MobRenderer<HamisEntity, HamisModel<HamisEntity>> {
     public  HamisRender(EntityRendererProvider.Context context) {
         super(context, new HamisModel(context.bakeLayer(HamisModel.LAYER_LOCATION)), 0.3f);
     }
--- src/main/java/com/github/vladyslavkhyzhniak/hamis/client/model/HamisModel.java
+++ src/main/java/com/github/vladyslavkhyzhniak/hamis/client/model/HamisModel.java
@@ -9,52 +9,87 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 
-import static com.github.vladyslavkhyzhniak.hamis.hamis.MOD_ID;
-
-public class HamisModel extends EntityModel<HamisEntity> {
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(MOD_ID, "hamis"), "main");
-
-	private final ModelPart boby;
+public class HamisModel<T extends HamisEntity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "hamis"), "main");
+	private final ModelPart root;
+	private final ModelPart body;
 	private final ModelPart legM;
-	private final ModelPart legR;
+	private final ModelPart leg01;
+	private final ModelPart leg02;
 	private final ModelPart legL;
+	private final ModelPart leg1;
+	private final ModelPart leg2;
+	private final ModelPart leg3;
+	private final ModelPart leg4;
+	private final ModelPart leg5;
+	private final ModelPart leg6;
+	private final ModelPart leg7;
+	private final ModelPart legR;
+	private final ModelPart bone;
+	private final ModelPart bone2;
+	private final ModelPart leg8;
 
 	public HamisModel(ModelPart root) {
-		this.boby = root.getChild("boby");
-		this.legM = root.getChild("legM");
-		this.legR = root.getChild("legR");
-		this.legL = root.getChild("legL");
+		this.root = root.getChild("root");
+		this.body = this.root.getChild("body");
+		this.legM = this.root.getChild("legM");
+		this.leg01 = this.legM.getChild("leg01");
+		this.leg02 = this.legM.getChild("leg02");
+		this.legL = this.root.getChild("legL");
+		this.leg1 = this.legL.getChild("leg1");
+		this.leg2 = this.legL.getChild("leg2");
+		this.leg3 = this.legL.getChild("leg3");
+		this.leg4 = this.legL.getChild("leg4");
+		this.leg5 = this.legL.getChild("leg5");
+		this.leg6 = this.legL.getChild("leg6");
+		this.leg7 = this.legL.getChild("leg7");
+		this.legR = this.root.getChild("legR");
+		this.bone = this.legR.getChild("bone");
+		this.bone2 = this.legR.getChild("bone2");
+		this.leg8 = this.legR.getChild("leg8");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition boby = partdefinition.addOrReplaceChild("boby", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -5.0F, -2.0F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
+		PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(-2.0F, 17.0F, -1.0F));
+
+		PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -5.0F, -2.0F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
 				.texOffs(2, 10).addBox(0.0F, -6.0F, -1.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(7, 9).addBox(0.0F, -2.0F, -1.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 20.0F, 0.0F));
+				.texOffs(7, 9).addBox(0.0F, -2.0F, -1.0F, 2.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 3.0F, 1.0F));
 
-		PartDefinition legM = partdefinition.addOrReplaceChild("legM", CubeListBuilder.create().texOffs(4, 6).addBox(-4.0F, -2.0F, -1.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(3.0F, 23.0F, -1.0F));
+		PartDefinition legM = root.addOrReplaceChild("legM", CubeListBuilder.create().texOffs(4, 6).addBox(0.0F, 2.0F, -1.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 2.0F, 0.0F));
 
-		legM.addOrReplaceChild("part01", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, -1.0F, 0.0F));
-		legM.addOrReplaceChild("part02", CubeListBuilder.create().texOffs(12, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, -2.0F, 0.0F));
+		PartDefinition leg01 = legM.addOrReplaceChild("leg01", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 3.0F, 0.0F));
 
-		PartDefinition legR = partdefinition.addOrReplaceChild("legR", CubeListBuilder.create(), PartPose.offset(3.0F, 22.0F, 2.0F));
-		legR.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(12, 10).addBox(-1.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.0F, -1.0F));
-		legR.addOrReplaceChild("bone2", CubeListBuilder.create().texOffs(8, 6).addBox(-1.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, -1.0F));
-		legR.addOrReplaceChild("part8", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, -1.0F));
+		PartDefinition leg02 = legM.addOrReplaceChild("leg02", CubeListBuilder.create().texOffs(12, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 2.0F, 0.0F));
 
+		PartDefinition legL = root.addOrReplaceChild("legL", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition legL = partdefinition.addOrReplaceChild("legL", CubeListBuilder.create(), PartPose.offset(0.0F, 23.0F, 0.0F));
-		legL.addOrReplaceChild("part1", CubeListBuilder.create().texOffs(12, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, -4.0F, 0.0F));
-		legL.addOrReplaceChild("part2", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, -3.0F, 0.0F));
-		legL.addOrReplaceChild("part3", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, -2.0F, 1.0F));
-		legL.addOrReplaceChild("part4", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, -2.0F, 1.0F));
-		legL.addOrReplaceChild("part5", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, -1.0F, 1.0F));
-		legL.addOrReplaceChild("part6", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 0.0F, 1.0F));
-		legL.addOrReplaceChild("part7", CubeListBuilder.create().texOffs(12, 8).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 1.0F, 1.0F));
+		PartDefinition leg1 = legL.addOrReplaceChild("leg1", CubeListBuilder.create().texOffs(12, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 2.0F, 1.0F));
+
+		PartDefinition leg2 = legL.addOrReplaceChild("leg2", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 3.0F, 1.0F));
+
+		PartDefinition leg3 = legL.addOrReplaceChild("leg3", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 4.0F, 2.0F));
+
+		PartDefinition leg4 = legL.addOrReplaceChild("leg4", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 4.0F, 2.0F));
+
+		PartDefinition leg5 = legL.addOrReplaceChild("leg5", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 5.0F, 2.0F));
+
+		PartDefinition leg6 = legL.addOrReplaceChild("leg6", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 6.0F, 2.0F));
+
+		PartDefinition leg7 = legL.addOrReplaceChild("leg7", CubeListBuilder.create().texOffs(12, 8).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.0F, 7.0F, 2.0F));
+
+		PartDefinition legR = root.addOrReplaceChild("legR", CubeListBuilder.create(), PartPose.offset(4.0F, 1.0F, 1.0F));
+
+		PartDefinition bone = legR.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(12, 10).addBox(-1.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 2.0F, 1.0F));
+
+		PartDefinition bone2 = legR.addOrReplaceChild("bone2", CubeListBuilder.create().texOffs(8, 6).addBox(-1.0F, 1.0F, -1.0F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, -1.0F, 1.0F));
+
+		PartDefinition leg8 = legR.addOrReplaceChild("leg8", CubeListBuilder.create().texOffs(0, 6).addBox(-1.0F, -2.0F, -1.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 4.0F, 1.0F));
 
 		return LayerDefinition.create(meshdefinition, 16, 16);
 	}
@@ -62,16 +97,10 @@ public class HamisModel extends EntityModel<HamisEntity> {
 	@Override
 	public void setupAnim(HamisEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 
-		this.legL.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-		this.legR.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-		this.legM.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
 	}
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		boby.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		legM.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		legR.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		legL.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		root.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
