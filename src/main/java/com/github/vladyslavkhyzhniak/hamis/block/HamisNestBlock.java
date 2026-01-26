d47ae1384869ff1776f49ae9a660e32d6df46e16
Vlad Kh <vladyslavkhyzhniak@gmail.com>
2026-01-26 02:54:44 +0100

    Added spawn hamis logic to the nest

--- src/main/java/com/github/vladyslavkhyzhniak/hamis/block/HamisNestBlock.java
+++ src/main/java/com/github/vladyslavkhyzhniak/hamis/block/HamisNestBlock.java
@@ -4,18 +4,24 @@ import com.github.vladyslavkhyzhniak.hamis.entity.HamisEntity;
 import com.github.vladyslavkhyzhniak.hamis.init.ModEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import java.util.List;
+import java.util.Random;
 
 public class HamisNestBlock extends Block {
 
+    private int spawnDelay = 20;
     public HamisNestBlock() {
         super(BlockBehaviour.Properties.of()
                 .strength(0.5f)
                 .sound(SoundType.SLIME_BLOCK)
+                .randomTicks()
         );
     }
 
@@ -23,23 +29,39 @@ public class HamisNestBlock extends Block {
     public void onRemove(BlockState state, Level level, BlockPos blockPos, BlockState blockState, boolean p_60519_) {
 
         if (state.getBlock() != blockState.getBlock()){
-            spawnHamisFamily((ServerLevel) level, blockPos);
+            SpawnHamis((ServerLevel) level,blockPos,2,5);
         }
         super.onRemove(state,level,blockPos,blockState,p_60519_);
 
     }
 
-    private void spawnHamisFamily(ServerLevel level, BlockPos blockPos) {
-        int count = level.random.nextInt(3) +3;
-        for (int i = 0; i<count; i++){
+    @Override
+    public void randomTick(BlockState blockState, ServerLevel level, BlockPos blockPos, RandomSource random) {
+        if (!level.hasNearbyAlivePlayer(blockPos.getX(), blockPos.getY(), blockPos.getZ(), 16)) {
+            return;
+        }
+        AABB checkArea = new AABB(
+                blockPos.getX(), blockPos.getY(),blockPos.getZ(),
+                blockPos.getX()+7,blockPos.getY()-20,blockPos.getZ()+7
+        );
+        List<HamisEntity> list =level.getEntitiesOfClass(HamisEntity.class, checkArea);
+        if (list.size() <13){
+            SpawnHamis((ServerLevel) level,blockPos,1,3);
+        }
+    }
+
+    private void SpawnHamis(ServerLevel level, BlockPos blockPos, int min, int max) {
+        Random random = new Random();
+        int amount = random.nextInt((max - min) + 1) + min;
+        for (int i = 0; i<amount; i++){
             HamisEntity hamis = ModEntities.HAMIS.get().create(level);
             if(hamis != null){
                 double x = blockPos.getX()+ 1 + (level.random.nextDouble()-0.5);
-                double y = blockPos.getY()+ 1;
+                double y = blockPos.getY() - 0.5;
                 double z = blockPos.getZ()+ 1 + (level.random.nextDouble()-0.5);
 
-            hamis.moveTo(x,y,z,level.random.nextFloat() * 360F,0);
-            level.addFreshEntity(hamis);
+                hamis.moveTo(x,y,z,level.random.nextFloat() * 360F,0);
+                level.addFreshEntity(hamis);
             }
         }
     }
