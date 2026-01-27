d0cf4c95731dfc2d0df3cea8998d5d324c93ba5f
Vlad Kh <vladyslavkhyzhniak@gmail.com>
2026-01-27 19:11:06 +0100

    fix: Added missing EventSubscriber

--- src/main/java/com/github/vladyslavkhyzhniak/hamis/ModEvents.java
+++ src/main/java/com/github/vladyslavkhyzhniak/hamis/ModEvents.java
@@ -4,7 +4,9 @@ import com.github.vladyslavkhyzhniak.hamis.init.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = "hamis", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
     @SubscribeEvent
     public static void buildContents(BuildCreativeModeTabContentsEvent event) {
