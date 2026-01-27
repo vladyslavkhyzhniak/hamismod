3bf152132459195a6b713cbb0a1a3f78bb61290a
Vlad Kh <vladyslavkhyzhniak@gmail.com>
2026-01-27 18:43:44 +0100

    Added spawn egg to creative tab

---
+++ src/main/java/com/github/vladyslavkhyzhniak/hamis/ModEvents.java
@@ -0,0 +1,15 @@
+package com.github.vladyslavkhyzhniak.hamis;
+
+import com.github.vladyslavkhyzhniak.hamis.init.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ModEvents {
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
+            event.accept(ModItems.HAMIS_SPAWN_EGG);
+        }
+    }
+}
